0afb8d79ccd612a39744e2fa76d1981ad96c2fad
Sushil Haridwar Girhepunje <73889131+sushilgirhepunje@users.noreply.github.com>
2021-12-26 18:02:30 +0530

    Create Multiplication_Table.java

---
+++ Sushil/Day 1/Multiplication_Table.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+
+	public static void main(String[] args) {
+		
+      Scanner sc = new Scanner(System.in);  // here we made and UserInput Interger
+      System.out.println("Enter Number:"); 
+
+      int number = sc.nextInt();
+      for(int i=1 ;i<=10; i++) {
+        // In this For Loop we start with 1 upto 10 and multiply it with the number UserInput while Running the Code
+    	  System.out.println(number +"*" + i + "=" + number *i);
+      }
+	}
+
+}
