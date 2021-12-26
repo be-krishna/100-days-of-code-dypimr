1feb0fb3abdec467ce4c1c4283073e202752d17c
Rushii07 <96677055+Rushii07@users.noreply.github.com>
2021-12-26 21:10:46 +0530

    Create Table.java

---
+++ Codder-Rushi07/Day-1/Table.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+ 
+class table
+{  
+    public static void main(String arg[])
+
+    {   
+        int n = 7; 
+          for (int i = 1; i <= 10; ++i) 
+          System.out.println(n + " * " + i + " = "
+           + n * i);
+    }
+}
