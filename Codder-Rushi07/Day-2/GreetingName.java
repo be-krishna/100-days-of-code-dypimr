737a98e1e0db02c2f879971fdb0b2c564c125afa
Rushii07 <96677055+Rushii07@users.noreply.github.com>
2021-12-26 21:00:02 +0530

    Commit

---
+++ Codder-Rushi07/Day-2/GreetingName.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.util.Scanner;
+ // Compiler version JDK 11.0.2
+
+ class Message
+ {
+   public static void main(String args[])
+   {
+     
+     Scanner sc =new Scanner (System.in);
+    System.out.println("Enter Your Name: ");
+    String Nm=sc.next();
+    System.out.println("HEY "+Nm);
+    
+   }
+ }
