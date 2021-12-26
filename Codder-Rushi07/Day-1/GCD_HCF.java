8232fcca5f246c78076227931b47a4f4d521ffff
Rushii07 <96677055+Rushii07@users.noreply.github.com>
2021-12-26 21:17:19 +0530

    Create GCD_HCF.java

---
+++ Codder-Rushi07/Day-1/GCD_HCF.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class GCDLCM
+{
+ public static void main(String args[])
+ {
+       int n1,n2;
+       int gcd,lcm,remainder,numerator,denominator;
+ Scanner sc = new Scanner(System.in);
+       System.out.println("Enter two Numbers ");
+       n1=sc.nextInt();
+ n2=sc.nextInt();
+       if (n1>n2)
+ { 
+         numerator=n1;
+         denominator=n2;
+     }
+     else
+     {
+       numerator=n2;
+       denominator=n1;
+       }
+       remainder=numerator%denominator;
+     while(remainder!=0)
+     {
+         numerator=denominator;
+        denominator=remainder;
+       remainder=numerator%denominator;
+     }
+     gcd = denominator;
+     lcm = n1*n2/gcd;
+     System.out.println("GCD of "+n1+" and "+n2+" = "+gcd);
+     System.out.println("LCM of "+n1+" and "+n2+" = "+lcm);
+ }
+}
