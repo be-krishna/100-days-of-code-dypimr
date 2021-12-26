3f985f5d60edae4dd394c78745389ede2c49d2b2
Rushii07 <96677055+Rushii07@users.noreply.github.com>
2021-12-26 21:05:44 +0530

    Commit new

--- Codder-Rushi07/Day-1/Sum.java
+++ Codder-Rushi07/Day-1/Sum.java
@@ -1,1 +1,11 @@
+import java.util.*;
 
+public class SumNumbers  
+{  
+public static void main(String args[])   
+{  
+int n1 = 20, n2 = 30, sum;  
+sum = n1 + n2;  
+System.out.println("The sum of numbers is: "+sum);  
+}  
+}
