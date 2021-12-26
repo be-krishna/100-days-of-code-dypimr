66d4205dea0f1fee0ed912f1d3f42985bce35dff
Sushil Haridwar Girhepunje <73889131+sushilgirhepunje@users.noreply.github.com>
2021-12-26 16:27:48 +0530

    Create Sum.java

---
+++ Sushil/Day 1/Sum.java
@@ -0,0 +1,14 @@
+public class SumClass {
+    public static void main(String args[]) {
+      int x=10;
+      int y=25;
+      int sum=x+y;
+
+      System.out.println("Sum of x + y is " + sum);
+    }
+}
+
+/*
+Here I created two integer x and y and assign them value 10 and 25.
+In the next step we Create new integer sum and Add x and y with the help of + Operator
+*/
