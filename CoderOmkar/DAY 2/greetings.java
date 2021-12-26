73252540f2b36af1270d8fc84a5ef35d7a5aaa92
CoderOmkar <70532604+CoderOmkar@users.noreply.github.com>
2021-12-26 19:53:27 +0530

    Create greetings.java

---
+++ CoderOmkar/DAY 2/greetings.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String a;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the name : ");
+        a= sc.next();
+
+        System.out.println("Good Morning "+a);
+    }
+}
