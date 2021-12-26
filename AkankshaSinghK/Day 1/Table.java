95040df912a905695e9ca9ab57259dc9ae20c2d4
AkankshaSinghK <96660041+AkankshaSinghK@users.noreply.github.com>
2021-12-26 14:29:38 +0530

    Table.java

---
+++ AkankshaSinghK/Day 1/Table.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+ import java.io.*;
+ 
+
+class table
+{  
+    public static void main(String arg[])
+
+    {   
+        int n = 5; 
+          for (int i = 1; i <= 10; ++i) 
+          System.out.println(n + " * " + i + " = "
+           + n * i);
+    }
+}
+ 
