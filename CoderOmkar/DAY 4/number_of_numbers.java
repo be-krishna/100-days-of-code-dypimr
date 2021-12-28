0c7af054d9ce4ea5b4816d93d1487d7b3e03fe3c
CoderOmkar <70532604+CoderOmkar@users.noreply.github.com>
2021-12-28 13:53:20 +0530

    Create number_of_numbers.java

---
+++ CoderOmkar/DAY 4/number_of_numbers.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //declare n initialize sum as 1
+        int n,sum=1,cn=0;
+        //scanner class
+        Scanner sc= new Scanner(System.in);
+
+       //while statement bcause we don't know the range
+        while (true) {
+            //taking user input
+            System.out.print("Write numbers :   ");
+            n = sc.nextInt();
+            //formula for sum of n numbers
+            sum=sum+n;
+            //if condition
+            if (n==-1) {
+                System.out.println("Thx! Bye!");
+                break;
+            }
+            //increase counter by 1
+            //after that we want to count number of n ,alternatively we also do cn+=1,(cn=cn+1)
+            cn++;
+        }
+        //prints sum of all user inputs
+        System.out.println("sum is : "+sum);
+        System.out.println("Numbers are : "+cn);
+    }
+}
