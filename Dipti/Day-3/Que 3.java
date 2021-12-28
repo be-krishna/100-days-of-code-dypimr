fb21e5abd6ed8def1b4ade3e73e2254a541f72ce
Dipti Chirutkar <76995277+DiptiChirutkar@users.noreply.github.com>
2021-12-28 23:08:20 +0530

    Create Que 3.java

---
+++ Dipti/Day-3/Que 3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class D1{
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int inputCount = 0;
+
+    while (true) {
+      System.out.println("Give a number:");
+      int input = Integer.valueOf(scanner.nextLine());
+
+      if (input == 0) {
+        break;
+      }
+
+      inputCount = inputCount + 1;
+    }
+
+    System.out.println("Number of numbers: " + inputCount);
+
+    
+  }
+}
