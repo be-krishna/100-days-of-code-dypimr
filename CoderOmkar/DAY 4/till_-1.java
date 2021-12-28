20c01b7f6e1343f265b6e93d83e115ba1d8a4a2c
CoderOmkar <70532604+CoderOmkar@users.noreply.github.com>
2021-12-28 12:57:42 +0530

    Create till_-1.java

---
+++ CoderOmkar/DAY 4/till_-1.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //declare n initialize cn as 0
+        int n,cn=0;
+        //scanner class
+        Scanner sc= new Scanner(System.in);
+
+       //while statement bcause we don't know the range
+        while (true) {
+            //taking user input
+            System.out.print("Write numbers :   ");
+            n = sc.nextInt();
+            //if n is equal to -1 then loop will be stop and prints the following statement
+            if (n == -1) {
+                System.out.println("Thx! Bye!");
+            }
+            
+        }
+
+
+    }
+}
