883dda6eae8c485e245653e988dd21ca1507d2fd
Vaishnavideshpande22 <96685805+Vaishnavideshpande22@users.noreply.github.com>
2021-12-28 12:05:59 +0530

    Create Add_two_nos.java

---
+++ Vaishnavi Deshpande/Day1/Add_two_nos.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+public class add_two_no {
+   public static void main(String[] args) {
+       int x,y, sum;
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter the first no:");
+       x = sc.nextInt();
+       System.out.println("Enter second no:");
+       y = sc.nextInt();
+       sum = x+y ;
+       System.out.println("Sum : " +sum);
+      
+
+       
+   } 
+}
