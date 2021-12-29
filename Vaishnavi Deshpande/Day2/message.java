4f835ede740d93041b4db8d164f41667a639ff37
Vaishnavideshpande22 <96685805+Vaishnavideshpande22@users.noreply.github.com>
2021-12-29 11:13:03 +0530

    Create message.java

---
+++ Vaishnavi Deshpande/Day2/message.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class message {
+    public static void main(String[] args) {
+
+		System.out.println("hello what is  your good name:");
+
+		Scanner input = new Scanner(System.in);
+		String name = input.next();
+
+		System.out.println("Hello my name is  " + name);
+
+	}
+}
