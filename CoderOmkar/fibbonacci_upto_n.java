44cfb006b1f22181a8db0de5170b02da7c97a515
CoderOmkar <70532604+CoderOmkar@users.noreply.github.com>
2021-12-29 21:11:31 +0530

    Create fibbonacci_upto_n.java

---
+++ CoderOmkar/fibbonacci_upto_n.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //declare and initialize elements
+        int n,first_num=0,second_num=1,next_num;
+
+
+        //scanner class
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the nth term : ");
+        n= sc.nextInt();
+
+        //using while loop bcoz we don't know the range
+        while(n>=first_num){
+
+            System.out.print(first_num+" ");
+            
+            //formula
+            next_num=first_num+second_num;
+            first_num=second_num;
+            second_num=next_num;
+
+
+        }
+
+
+    }
+}
