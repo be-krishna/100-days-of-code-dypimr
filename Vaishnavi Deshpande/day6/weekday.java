ae15cd7e2f824de777e0f8102f7400878ac574e4
Vaishnavideshpande22 <96685805+Vaishnavideshpande22@users.noreply.github.com>
2022-01-02 15:16:26 +0530

    Create weekday.java

---
+++ Vaishnavi Deshpande/day6/weekday.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class week3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter weekday day number  : ");
+        int weekday = sc.nextInt();
+
+        if(weekday == 2) {
+
+            System.out.println("Monday");
+
+        } else if(weekday == 3) {
+
+            System.out.println("Tuesday");
+
+        } else if(weekday == 4) {
+
+            System.out.println("Wednesday");
+
+        } else if(weekday == 5) {
+
+            System.out.println("Thursday");
+
+        } else if(weekday == 6) {
+
+            System.out.println("Friday");
+
+        } else if(weekday == 7) {
+
+            System.out.println("Saturday");
+
+        } else if(weekday == 1) {
+
+            System.out.println("Sunday");
+
+        } else {
+
+            System.out.println("Please enter weekday number .");
+        }
+
+    }
+}
