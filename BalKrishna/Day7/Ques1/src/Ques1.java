9885b7d24ae43a65f5b6d8d56aeb8d8e3feb7a9b
balKrishna <bkshgnj@gmail.com>
2022-01-02 10:10:18 +0530

    Completed Ques1

--- BalKrishna/Day7/Ques1/src/Ques1.java
+++ BalKrishna/Day7/Ques1/src/Ques1.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Ques1 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input number of rows: ");
+        int rows = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <= rows; i++) {
+            System.out.println();
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+        }
+
+        scanner.close();
     }
 }
